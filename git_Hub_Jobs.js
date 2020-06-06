698aac23bc1233ecfdb1372e4e90fc0f05514aa9
Misal-Ambasta <misalambasta@gmail.com>
2020-06-06 17:16:39 +0530

    Date posted function added

--- git_Hub_Jobs.js
+++ git_Hub_Jobs.js
@@ -2,6 +2,18 @@
 // Result = where to show all the result found
 var result = document.getElementById('result')
 
+ 
+
+function dateDiffInDays(a, b) 
+{
+  // Discard the time and time-zone information.
+  var postedDate = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    //   console.log(postedDate+'posted')
+  var currentDate = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((currentDate - postedDate) / (1000 * 60 * 60 * 24));
+}
+
 
 function renderJobs(data)
 {
@@ -16,9 +28,11 @@ function renderJobs(data)
     var jobtype
     var company_url
 
+    
+    
     var jobs = data[0]
       
-    // console.log(total_jobs,jobs)
+    console.log(jobs)
 
     result.style.marginTop="25px"
 
@@ -29,6 +43,10 @@ function renderJobs(data)
 
     result.append(div_total_job) // appending total jobs
 
+    var current_date = new Date()
+
+    
+    console.log(current_date)
     //loop through object
 
     for(var i=0; i<total_jobs; i++)
@@ -43,7 +61,13 @@ function renderJobs(data)
         company_logo_url = data[i].company_logo
         // console.log(company)
 
-        
+        // Posted Date difference Calculation
+        created_at=data[i].created_at
+        created_at = new Date(created_at)
+
+        var difference = dateDiffInDays(created_at, current_date);
+        console.log(typeof difference )
+
         var para_title = document.createElement('a')    // a tag for job title
         var div = document.createElement('div')     // data to be render in this box
         var div_inner = document.createElement('div') // inner box for all text 
@@ -52,8 +76,18 @@ function renderJobs(data)
         var div_jobtype =document.createElement('div')
         var logo_img = document.createElement('img')    //for company logo
         var location_div = document.createElement('div')
-
+        var posted_day_before = document.createElement('div')
         
+        // date 
+        if(difference!=1)
+        {
+            posted_day_before.textContent=difference+" days ago Posted"
+        }
+        else{
+            posted_day_before.textContent=difference+" day ago Posted"
+        }
+        
+        posted_day_before.style.color="white"
 
         div.style.margin="10px"
         div.style.padding="10px"
@@ -73,7 +107,7 @@ function renderJobs(data)
 
         //Location of job details
         location_div.textContent=location;
-        location_div.style.color="crimson"
+        location_div.style.color="rgb(29, 163, 96)"
 
         //Jobtype details
         div_jobtype.textContent=jobtype
@@ -93,7 +127,7 @@ function renderJobs(data)
 
 
         // appending data on inner box which stores all texts
-        div_inner.append(para_title,p_Cname,div_jobtype,location_div)
+        div_inner.append(para_title,p_Cname,div_jobtype,location_div,posted_day_before)
 
         // appending inner and logo box
         div.append(div_inner,img_div)
